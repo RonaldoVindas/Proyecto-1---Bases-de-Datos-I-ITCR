aaae6adfbd30c5380b41c5d4af03a58d895cf9d6
RonaldoVindas <44733416+RonaldoVindas@users.noreply.github.com>
2020-06-29 19:39:42 -0600

    Entire User Interface created and uploaded.

---
+++ User Interface/CRIMELOGS/src/crimelogs/CRIMELOGS.java
@@ -0,0 +1,17 @@
+
+package crimelogs;
+
+/**
+ *
+ * @author Ronaldo
+ */
+public class CRIMELOGS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
