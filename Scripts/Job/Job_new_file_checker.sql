2e090260c685ae9e65becd5c117d8a2828a81e22
Diego Rodriguez <ldrc161997@gmail.com>
2020-07-01 16:41:56 -0600

    connect DB procedures. all teammates worked on this

--- Scripts/Job/Job_new_file_checker.sql
+++ Scripts/Job/Job_new_file_checker.sql
@@ -2,8 +2,8 @@ BEGIN
     DBMS_SCHEDULER.create_job (
         job_name => 'DAILY_NEW_FILE_CHECK',
         job_type => 'PLSQL_BLOCK',
-        job_action => 'BEGIN CONTROL_JOBCHECKCOUNT.JOBCHECKCOUNT; END;',
-        start_date => '27-JUN-20 11.00.00 PM -06:00',
+        job_action => 'BEGIN SELECT CONTROL_QUERIES.NEWCRIMINALRECORD FROM DUAL; END;',
+        start_date => '30-JUN-20 11.12.00 PM -06:00',
         repeat_interval => 'FREQ=DAILY;INTERVAL=1',
         end_date => NULL,
         enabled => TRUE,
